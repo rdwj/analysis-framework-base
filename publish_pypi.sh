7abdaf4e6a1f2218676bb2034e2df9e9679dddb9
rdwj <wjackson@redhat.com>
2025-10-28 21:58:23 -0500

    Add PyPI publishing script

---
+++ publish_pypi.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script is used to publish the analysis-framework-base to PyPI.
+# Clean previous builds to avoid uploading old versions
+rm -rf dist/ build/
+
+python -m build
+
+# Upload to PyPI using either local .pypirc, global .pypirc, or TWINE_PASSWORD environment variable
+if [ -f ".pypirc" ]; then
+    python -m twine upload --config-file .pypirc dist/*
+elif [ -n "$TWINE_PASSWORD" ]; then
+    python -m twine upload --username __token__ --password "$TWINE_PASSWORD" dist/*
+else
+    python -m twine upload dist/*
+fi
